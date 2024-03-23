bd3ab152cac303a2ace5bb2c1c7dfdea7d021446
Ainur <sibrinat616@gmail.com>
2024-03-23 10:21:20 +0300

    git init

---
+++ AnimationSwiftUI/AnimationSwiftUI/AnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUIApp.swift
+//  AnimationSwiftUI
+//
+//  Created by Ainur on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
